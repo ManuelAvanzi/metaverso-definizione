9c9b333eb5bcb905ede39ea6fd9e782827337cc3
ManuelAvanzi <manuel.92.avanzi@gmail.com>
2022-11-09 15:33:55 +0100

    start js

--- main/script.js
+++ main/script.js
@@ -1,1 +1,14 @@
-console.log("buongiornissimo");
+const a=document.getElementById("a");
+const b=document.getElementById("b");
+const c=document.getElementById("c");
+const d=document.getElementById("d");
+const e=document.getElementById("e");
+const f=document.getElementById("f");
+const g=document.getElementById("g");
+const h=document.getElementById("h");
+const i=document.getElementById("i");
+
+
+a.addEventListener("click",function(){
+    document.querySelector(".yellow").style.backgroundColor="red"
+})
